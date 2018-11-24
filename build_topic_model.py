1a652e4fa4ef0ce650195cbbd9363eea88dabc81
Motaz Saad <motaz.saad@gmail.com>
2018-11-24 05:19:13 +0000

    fix bug

--- build_topic_model.py
+++ build_topic_model.py
@@ -33,7 +33,7 @@ def load_plain_corpus(corpus_dir):
     
 def build_model(corpus, corpus_name, min_freq, topics):
     print('min freq:', min_freq, 'topics:', topics)
-    texts = [[word for word in d.split()] for i, t, d  in corpus]
+    texts = [[word for word in d.split()] for d  in corpus]
     print('texts collected')
     frequency = defaultdict(int)
     for text in texts:
